a38753c5a4354cb71642d02c0d8b13ddca6426e5
Sirzbear <zacharyhan99@gmail.com>
2026-07-13 13:31:51 -0400

    better lab 2 ms

---
+++ lab02/tests/q6newnew.py
@@ -0,0 +1,24 @@
+test = {
+  'name': '6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> int(temperature_K)
+          300
+          >>> int(volume_L)
+          24
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
